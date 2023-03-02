0188a9edd8ce9f967f716f751649991436202d5c
mimo-oo <fadipemichael6@gmail.com>
2023-03-02 11:51:37 -0800

    first js

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function hide() {
+    const x = document.getElementById("hidden")
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
