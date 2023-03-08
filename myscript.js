6bc30331d14fcd18efadf5f2ad3ca882a6d101ec
mimo-oo <fadipemichael6@gmail.com>
2023-03-08 12:43:52 -0800

    i updated the dropdown menu

--- myscript.js
+++ myscript.js
@@ -6,3 +6,23 @@ function hide() {
         x.style.display = "block"
     }
 }
+
+function display() {
+    const xc = document.getElementById("state");
+    xc.style.display = "block"
+}
+
+function notdisplay() {
+    const c = document.getElementById("state");
+    c.style.display = "none"
+}
+
+function adisplay() {
+    const cb = document.getElementById("sort");
+    cb.style.display = "block"
+}
+
+function anotdisplay() {
+    const ca = document.getElementById("sort");
+    ca.style.display = "none"
+}
